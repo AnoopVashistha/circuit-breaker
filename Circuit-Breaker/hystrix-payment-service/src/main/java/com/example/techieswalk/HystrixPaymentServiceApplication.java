ae3622ce29f37dae07b2f61f374ed5461be73480
Anoop Kumar Vashistha <43183838+AnoopVashistha@users.noreply.github.com>
2021-09-05 22:31:25 +0530

    Add files via upload

---
+++ Circuit-Breaker/hystrix-payment-service/src/main/java/com/example/techieswalk/HystrixPaymentServiceApplication.java
@@ -0,0 +1,11 @@
+package com.example.techieswalk;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HystrixPaymentServiceApplication {
+	public static void main(String[] args) {
+		SpringApplication.run(HystrixPaymentServiceApplication.class, args);
+	}
+}
